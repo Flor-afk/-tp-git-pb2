a9d0a13b1abbc14581e8835fe7fbdfc223b2307e
Flor-afk <florfarfan04@gmail.com>
2024-08-28 16:25:26 -0300

    Se agrega switch en el main

--- test/ar/edu/unlam/github/dom/CalculadoraTest.java
+++ test/ar/edu/unlam/github/dom/CalculadoraTest.java
@@ -16,25 +16,50 @@ public class CalculadoraTest {
 		
 		do{
 			
-		System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+		System.out.println("Ingrese su operación \n1 para sumar \n2 para restar \n3 para multiplicar \n4 para dividir");
 		opcion = teclado.nextInt();
-		
-		}while(opcion < 1 && opcion > 4);
 		System.out.println("Ingrese el primer numero");
 		a = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero");
 		b = teclado.nextInt();
 		
-		if(opcion.equals(1)) {
-		resultado = a + b;
-		}else if(opcion.equals(2)){
-		resultado = a - b;
-		}else if(opcion.equals(3)){
-		resultado = a * b;
-		}else{
-		resultado = a / b;
+		switch(opcion) {
+		
+		case 1:
+			resultado = a * b;
+			System.out.println("El resultado es " + resultado.toString());
+			break;
+		case 2:
+			resultado = a / b;
+			System.out.println("El resultado es " + resultado.toString());
+			break;
+		case 3:
+			resultado = a + b;
+			System.out.println("El resultado es " + resultado.toString());
+			break;
+		case 4:
+			resultado = a - b;
+			System.out.println("El resultado es " + resultado.toString());
+			break;
 		}
-		System.out.println("El resultado es " + resultado.toString());
+		}while(opcion < 1 && opcion > 4);
+		
+//		System.out.println("Ingrese el primer numero");
+//		a = teclado.nextInt();
+//		System.out.println("Ingrese el segundo numero");
+//		b = teclado.nextInt();
+//		
+//		if(opcion.equals(1)) {
+//		resultado = a + b;
+//		}else if(opcion.equals(2)){
+//		resultado = a - b;
+//		}else if(opcion.equals(3)){
+//		resultado = a * b;
+//		}else{
+//		resultado = a / b;
+//		}
+//		
+//		System.out.println("El resultado es " + resultado.toString());
 		
 	}
 }
